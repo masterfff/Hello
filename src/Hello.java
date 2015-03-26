d3a826029483861895ee88485940df846ab5b33c
Alexej <Alexej@192.168.101.136>
2015-03-26 15:30:14 +0100

    third

--- src/Hello.java
+++ src/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Alex!!");
+System.out.println("Hello Alex and friends!!");
 
 	}
 
